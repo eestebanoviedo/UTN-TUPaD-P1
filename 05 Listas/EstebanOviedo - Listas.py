7ef52e8897254c04b77edb74fe174e447ff9cce5
Esteban Oviedo <esteban@imajine.com>
2025-05-10 14:33:19 -0300

    listas

---
+++ 05 Listas/EstebanOviedo - Listas.py
@@ -0,0 +1,77 @@
+# -------- Ejercicio 1 --------
+
+# numeros_multiplos_4 = list(range(4, 101, 4))
+# print(numeros_multiplos_4)
+
+# -------- Ejercicio 2 --------
+
+# lista = [1, 2, 3, 4, 5]
+# print(lista[-2])
+
+# -------- Ejercicio 3 --------
+
+# lista_vacia = []
+# lista_vacia.append("Hola")
+# lista_vacia.append("Soy")
+# lista_vacia.append("Esteban")
+# print(lista_vacia)
+
+# -------- Ejercicio 4 --------
+
+# animales = ["perro","gato","conejo","pez"]
+
+# animales[1] = "loro"
+# animales[-1] = "oso"
+# print(animales)
+
+# -------- Ejercicio 5 --------
+
+# numeros = [8, 15, 3, 22, 7]
+# numeros.remove(max(numeros))
+# print(numeros)
+
+# Linea1: Define una lista vacía llamada "numeros" 
+# Linea2: Busca el numero mayor en la lista "numeros" y lo elimina
+# Linea3: Imprime la lista "numeros" sin el numero mayor
+
+# -------- Ejercicio 6 --------
+
+# numeros = list(range(10, 31, 5))
+# print(numeros[:2])
+
+# -------- Ejercicio 7 --------
+
+# 7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
+# cualesquiera.
+# autos = ["sedan", "polo", "suran", "gol"]
+
+# autos[1] = "fiesta"
+# autos[2] = "focus"
+
+# -------- Ejercicio 8 --------
+
+# dobles = []
+# dobles.append(5 * 2)
+# dobles.append(10 * 2)
+# dobles.append(15 * 2)
+
+# -------- Ejercicio 9 --------
+
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+# compras[2].append("jugo")
+# compras[1][1] = "tallarines"
+# compras[0].remove("pan")
+
+# -------- Ejercicio 10 --------
+
+# lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+# print(lista_anidada)
+
+
+
+
+
+
+
+
+
